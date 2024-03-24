14d96343311ff07b2a04409b15db1cff5c9f8ebc
mohdaamil120 <mohdaamil120@gmail.com>
2024-03-24 12:11:58 +0530

    changes done

--- db.js
+++ db.js
@@ -4,9 +4,6 @@ require("dotenv").config()
 // MongoDb Atlas
 const connection = mongoose.connect(process.env.mongoURL)
 
-// Local mongoDB
-// const connection = mongoose.connect(process.env.LOCALDB)
-
 
 module.exports = {
     connection
--- index.js
+++ index.js
@@ -32,3 +32,4 @@ app.listen(8080, async()=>{
 })
 
 
+
